3dea07a913087c264ef683bcf942f46c7a26edaa
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2019-11-14 13:17:09 +0530

    add recipe-list and recipe-detail design

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,7 +8,10 @@ import { Recipe } from '../model/recipe.model';
 })
 export class RecipesListComponent implements OnInit {
 
-  recipes:Recipe[] =[]
+  recipes:Recipe[] =[
+    new Recipe('recipe-name1','recipe-desc1','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQy-VYIUlJc-emqtqidd5qiRPrWFOvRqyputvKybENnZ6lZjArP'),
+    new Recipe('recipe-name2','recipe-desc2','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQy-VYIUlJc-emqtqidd5qiRPrWFOvRqyputvKybENnZ6lZjArP')
+  ]
   constructor() { }
 
   ngOnInit() {
